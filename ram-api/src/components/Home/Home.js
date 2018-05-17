e2e2588bb56be3e45f6d99872842648a1ae3732f
P Trekell <pavel.trekell@gmail.com>
2018-05-16 18:57:27 -0700

    added prealoding of all data for the home page

--- ram-api/src/components/Home/Home.js
+++ ram-api/src/components/Home/Home.js
@@ -10,18 +10,49 @@ class Home extends Component {
     }
 
 
-    componentDidMount() {
+    sortCharactersAlph = () => {
+        let characters = this.state.characters.sort(function (a, b) {
+            if (a.name < b.name) return -1;
+            if (a.name > b.name) return 1;
+            return 0;
+        });
 
-        fetch('https://rickandmortyapi.com/api/character/')
-            .then(response => response.json())
-            .then(data => {
-                this.setState(prevState => {
-                    return {
-                        characters: data.results
-                    }
-                })
-            })
-            .catch(error => console.log(error));
+        this.setState(prevState => {
+            return {
+                characters: characters
+            }
+        })
+    }
+
+    componentDidMount() {
+        let allCharacters = [];
+        let initalJsonPullPromise = new Promise((resolve, reject) => {   //first load the "pages" value from the info object of the character API point
+            fetch('https://rickandmortyapi.com/api/character/')
+                .then(response => response.json())
+                .then(data => resolve(data.info.pages))                  //resolve the promise with the actual page count
+        })
+        initalJsonPullPromise.then(pageCount => {
+            let promiseArr = [];
+            while (pageCount >= 1) {
+                promiseArr.push( //new promise/fetch for each page
+                    fetch('https://rickandmortyapi.com/api/character/?page=' + pageCount)
+                        .then(response => response.json())
+                        .then(data => data.results.forEach(character => allCharacters.push(character)))
+                );
+                pageCount--;
+            }
+            Promise.all(promiseArr)
+                .then(allCombined => { //pushing individual characters into the allCharacters array above to avoid nested for loops as allCombined is array of arrays...
+                    this.setState(prevState => {
+                        return {
+                            characters: allCharacters
+                        }
+                    })
+                    this.sortCharactersAlph();
+                });
+
+        })
+
     }
 
 
@@ -29,15 +60,15 @@ class Home extends Component {
 
 
         let filteredCards = this.state.characters.filter(character => {
-            return character.name.toLowerCase().indexOf(this.props.searchBoxValue.toLowerCase()) !== -1;
+            return character.name ? character.name.toLowerCase().indexOf(this.props.searchBoxValue.toLowerCase()) !== -1 : null;
         });
 
         return (
             <div className="Home">
                 {
                     filteredCards.map(character => {
-                       
-                        return <Card key={character.id} character={character}/>;
+
+                        return <Card key={character.id} character={character} />;
                     })
                 }
 
