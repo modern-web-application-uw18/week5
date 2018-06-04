2d4cc5e1c77a644680e9c1689ea062f89e5b6545
Brent Schneider <brentschneider@Brents-MacBook-Pro.local>
2018-06-03 23:52:32 -0700

    submit for feeedback

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -67,7 +67,7 @@ constructor(props){
               </div>;
             })}
 
-           <Route path="/" />
+          <Route path="/" />
           <Route path="/people" Component={thePeople} />
          
         </span>
